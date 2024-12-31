3ada06cf77ee6d7aa0f112e7950d5cbffa6ef661
WilliamPyke <pykewilliam1@gmail.com>
2024-12-31 01:28:40 -0400

    remove unused definition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata as NextMetadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { LoadingScreen } from "@/components/loading";
 import "./globals.css";
